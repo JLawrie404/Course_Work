2327d4e7b3bfc6d978f9ef4389d48728e51a59b2
Iapetus411 <40495613@live.napier.ac.uk>
2022-05-06 01:37:04 +0100

    Question Details and questions Final tie in

---
+++ questions.js
@@ -0,0 +1,94 @@
+//Creates an array to hold data such as quiz questions, question number, possible answers and correct answer
+//A seperate file is used to increase readability of project and allow for any future changes
+
+
+let questions = [
+    {
+    numb: 1,
+    question: "What is the approximate number of computing languages currently in use?",
+    answer: "A : 2000",
+    options: [
+        "A : 2000",
+        "B : 2500",
+        "C : 1000",
+        "D : 4000"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is an early computer?",
+    answer: "C : UNIVAC",
+    options: [
+        "A : NASA",
+        "B : ENIVIE",
+        "C : UNIVAC",
+        "D : FSB"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who founded Samsung?",
+    answer: "A : Lee Byung-Chul",
+    options: [
+        "A : Lee Byung-Chul",
+        "B : Steven Jobs",
+        "C : Elon Musket",
+        "D : Alice Greener"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What was not possible until the late 1990s?",
+    answer: "A : Blogging",
+    options: [
+        "A : Blogging",
+        "B : Telnet",
+        "C : Gopher",
+        "D : FTP"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is the oldest Social Media site?",
+    answer: "A : Six Degrees",
+    options: [
+        "A : Six Degrees",
+        "B : Facebook",
+        "C : Twitter",
+        "D : MySpace "
+    ]
+  },
+    {
+    numb: 6,
+    question: "Identify the odd one out?",
+    answer: "D : Lada",
+    options: [
+        "A : Apple",
+        "B : Lenovo",
+        "C : Toshiba",
+        "D : Lada"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which of these is not an Apple product?",
+    answer: "A : IMAX",
+    options: [
+        "A : IMAX",
+        "B : iPhone",
+        "C : iMac",
+        "D : iPod"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What is the latest version of Windows?",
+    answer: "B : Windows 12",
+    options: [
+        "A : Windows 11",
+        "B : Windows 12",
+        "C : Windows NT",
+        "D : Windows XP"
+    ]
+  },
+];
